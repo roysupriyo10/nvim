2b62d7afadb1a3df9deac915911492b213508ea1
roysupriyo10 <roysupriyo10@gmail.com>
2026-04-19 18:50:11 +0530

    feat: add nordic theme

---
+++ lua/plugin/themes/nordic.lua
@@ -0,0 +1,38 @@
+return {
+	"AlexvZyl/nordic.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {
+		bold_keywords = true,
+		italic_comments = true,
+		transparent = {
+			bg = true,
+			float = true,
+		},
+		bright_border = true,
+		reduced_blue = true,
+		swap_backgrounds = false,
+		cursorline = {
+			bold = false,
+			bold_number = true,
+			theme = "dark",
+			blend = 0.85,
+		},
+		visual = {
+			bold = false,
+			bold_number = true,
+			theme = "dark",
+			blend = 0.85,
+		},
+		noice = {
+			style = "classic",
+		},
+		telescope = { style = "flat" },
+		leap = {
+			dim_backdrop = false,
+		},
+		ts_context = {
+			dark_background = true,
+		},
+	},
+}
